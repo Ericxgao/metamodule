fae63ea9692d6b0e7a73f6e0ae8797a35b5fc7f7
Dan Green <danngreen1@gmail.com>
2024-07-29 12:33:09 -0700

    Start filesystem access wrapper

--- shared/CoreModules/4ms/core/sampler/mocks/brain_conf.hh
+++ shared/CoreModules/4ms/core/sampler/mocks/brain_conf.hh
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <cstdint>
 namespace SamplerKit::Brain
 {
 
-static constexpr int MinPotChange = 1;
-static constexpr int MinCVChange = 1;
+inline constexpr int MinPotChange = 1;
+inline constexpr int MinCVChange = 1;
+inline constexpr uint32_t MemorySizeBytes = 8 * 1024 * 1024;
+inline constexpr float AudioGain = 1.f;
 
 } // namespace SamplerKit::Brain
