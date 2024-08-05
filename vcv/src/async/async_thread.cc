9aed6579c04ad8d0e5953a387def078b317e62cf
Dan Green <danngreen1@gmail.com>
2024-08-05 10:26:00 -0700

    async_thread uses bridge to implementation:
    - id for hardware
    - std::thread for VCV and simulator

--- vcv/src/async/async_thread.cc
+++ vcv/src/async/async_thread.cc
@@ -1,46 +1,52 @@
 #include "CoreModules/async_thread.hh"
-#include <array>
-#include <list>
 #include <thread>
 
 namespace MetaModule
 {
 
+struct AsyncThread::Internal {
+	bool enabled = false;
+	std::thread thread;
+};
+
 namespace //anonymous
 {
 
-//TODO: use pImpl with AsyncThread
-// Rack and sim Impl will have a std::thread
-// Firmware Impl will have an id in a static array
-
-static std::list<std::thread> threads;
-
-static std::atomic<bool> kill_all_signal = false;
-static std::atomic<bool> pause_all_signal = false;
+std::atomic<bool> kill_signal = false;
+std::atomic<bool> pause_signal = false;
 
 } // namespace
 
-AsyncThread::AsyncThread() = default;
+AsyncThread::AsyncThread()
+	: internal{new AsyncThread::Internal} {
+}
 
 AsyncThread::AsyncThread(Callback &&new_action)
-	: action(std::move(new_action)) {
-	printf("New thread, no id\n");
+	: action(std::move(new_action))
+	, internal{new AsyncThread::Internal} {
+}
+
+AsyncThread::~AsyncThread() {
+	internal->enabled = false;
+	internal->thread.join();
 }
 
 void AsyncThread::start(unsigned module_id) {
-	if (action && module_id > 0) {
-		id = module_id;
-		printf("Start thread %d\n", id);
-
-		threads.emplace_back([this]() {
-			while (!kill_all_signal && id != 0) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(1));
-				if (!pause_all_signal)
-					action();
-			}
-			// printf("Died\n");
-		});
+	if (!action) {
+		internal->enabled = false;
+		return;
 	}
+
+	internal->enabled = true;
+
+	internal->thread = std::thread([this]() {
+		while (internal->enabled && !kill_signal) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+			if (!pause_signal)
+				action();
+		}
+	});
 }
 
 void AsyncThread::start(unsigned module_id, Callback &&new_action) {
@@ -48,30 +54,17 @@ void AsyncThread::start(unsigned module_id, Callback &&new_action) {
 	start(module_id);
 }
 
-AsyncThread::~AsyncThread() {
-	printf("Kill thread %d\n", id);
-	id = 0; //kill
-
-	// how to wait for it to die?
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
-}
-
 void start_module_threads() {
-	// printf("Starting threads...\n");
-	kill_all_signal = false;
+	kill_signal = false;
+	pause_signal = false;
 }
 
 void pause_module_threads() {
-	pause_all_signal = true;
+	pause_signal = true;
 }
 
 void kill_module_threads() {
-	printf("Killing threads...\n");
-	kill_all_signal = true;
-
-	for (auto &t : threads) {
-		t.join();
-	}
+	kill_signal = true;
 }
 
 } // namespace MetaModule
