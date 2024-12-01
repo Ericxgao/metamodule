50ba136af8cfef2674c899b7186563718fb9b81a
Dan Green <danngreen1@gmail.com>
2024-11-30 17:35:24 -0800

    M4 does not get newlib fs implementations -- use stubs only

---
+++ firmware/src/core_m4/libc_stub.c
@@ -0,0 +1,19 @@
+int _open(const char *filename, int flags, int mode) {
+	return -1;
+}
+
+int _close(int fd) {
+	return -1;
+}
+
+int _lseek(int fd, int offset, int whenc) {
+	return -1;
+}
+
+int _read(int fd, char *ptr, int len) {
+	return -1;
+}
+
+int _isatty(int fd) {
+	return 0;
+}
