583e313c32d2e6a324abca0398af86d74e276ba2
Dan Green <danngreen1@gmail.com>
2024-12-02 15:39:00 -0800

    Cmake: always build with CONSOLE_USE_USB enabled

--- firmware/src/core_a7/main.cc
+++ firmware/src/core_a7/main.cc
@@ -109,11 +109,9 @@ void main() {
 	pr_info("A7 Core 1 initialized\n");
 	// Note: from after the HAL_Delay(50) until here, it takes 20ms
 
-#ifdef CONSOLE_USE_USB
-	printf("Stopping UART buffer\n");
+	pr_dbg("Stopping UART buffer\n");
 	UartLog::use_usb(&StaticBuffers::console_a7_0_buff);
-	printf("Using USB buffer\n");
-#endif
+	pr_dbg("Using USB buffer\n");
 
 	// Tell other cores we're done with init
 	mdrivlib::HWSemaphore<MainCoreReady>::unlock();
