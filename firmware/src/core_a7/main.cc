ba3b8c4bb40afe1a58ce04d385692abb150c3f20
Dan Green <danngreen1@gmail.com>
2024-12-03 11:37:39 -0800

    Revert "Cmake: always build with CONSOLE_USE_USB enabled"
    This reverts commit 583e313c32d2e6a324abca0398af86d74e276ba2.

--- firmware/src/core_a7/main.cc
+++ firmware/src/core_a7/main.cc
@@ -101,9 +101,11 @@ void main() {
 	pr_info("A7 Core 1 initialized\n");
 	// Note: from after the HAL_Delay(50) until here, it takes 20ms
 
-	pr_dbg("Stopping UART buffer\n");
+#ifdef CONSOLE_USE_USB
+	printf("Stopping UART buffer\n");
 	UartLog::use_usb(&StaticBuffers::console_a7_0_buff);
-	pr_dbg("Using USB buffer\n");
+	printf("Using USB buffer\n");
+#endif
 
 	// Tell other cores we're done with init
 	mdrivlib::HWSemaphore<MainCoreReady>::unlock();
